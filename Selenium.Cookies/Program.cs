3bb740d1af9c0408f41a2d0c47be5ca1330d16ed
Demchenko Kirill <thegalaxydias@gmail.com>
2021-10-19 20:16:06 +0300

    Добавьте файлы проекта.

---
+++ Selenium.Cookies/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace Selenium.Cookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://itechcraft.com/");
+
+            driver.Manage().Cookies.AddCookie(new Cookie("lol", "loooool"));
+        
+            Cookie cookie = driver.Manage().Cookies.GetCookieNamed("lol");
+           
+            Console.WriteLine("KYKU = " + cookie.Value);
+            driver.Quit();
+        }
+    }
+}
